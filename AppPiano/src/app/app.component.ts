a8a4a1eb5a3a11526c937a488ccb3ac77e40fdec
JuanFernandezJubin <fernandezjubin.data@gmail.com>
2021-05-11 10:28:11 -0300

    simple app piano to practice in Angular

---
+++ AppPiano/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AppPiano';
+
+  aplicarSonido(sound: number): void {
+    const audio = new Audio();
+    audio.src = '../assets/sounds/note' + sound + '.wav';
+    audio.load();
+    audio.play();
+  }
+}
